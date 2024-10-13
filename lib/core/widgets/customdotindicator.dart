d746f2ddbf2c57fe46c90d6ce1e40fb3f0e5deba
Abdulaziz Abaza <abdulazizabaza@gmail.com>
2024-10-13 16:44:13 +0300

    13/100

--- lib/core/widgets/customdotindicator.dart
+++ lib/core/widgets/customdotindicator.dart
@@ -2,10 +2,12 @@ import 'package:artificial_app/constant/constants.dart';
 import 'package:artificial_app/core/utils/sizeconfig.dart';
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Customdotindicator extends StatelessWidget {
   const Customdotindicator({super.key, required this.dotIndex});
   final int dotIndex;
+
   @override
   Widget build(BuildContext context) {
     return Positioned(
@@ -14,16 +16,26 @@ class Customdotindicator extends StatelessWidget {
       bottom: Sizeconfig.defaultSize! * 20,
       child: DotsIndicator(
         decorator: DotsDecorator(
-            size: Size(
-                Sizeconfig.defaultSize! * 2.5, Sizeconfig.defaultSize! * 0.6),
-            activeColor: AppColors.primaryColor,
-            color: Colors.transparent,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(3),
-                side: BorderSide(color: AppColors.primaryColor))),
+          size: Size(
+            Sizeconfig.defaultSize! * 2.5.sp,
+            Sizeconfig.defaultSize! * 0.6.sp,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(3),
+            side: BorderSide(color: AppColors.primaryColor),
+          ),
+          color: Colors.transparent,
+          activeSize: Size(
+            Sizeconfig.defaultSize! * 2.5.sp,
+            Sizeconfig.defaultSize! * 0.6.sp,
+          ),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(3),
+            side: BorderSide(color: AppColors.primaryColor),
+          ),
+          activeColor: AppColors.primaryColor,
+        ),
         dotsCount: 3,
-        //  position:0,
-        //  position: dotIndex,
         position: dotIndex,
       ),
     );
